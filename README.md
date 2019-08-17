ff7e1ceccdcd2605daa96b9139e000d7731359a6
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-17 14:41:24 +0430

    Added vendor-shared patches

--- README.md
+++ README.md
@@ -14,4 +14,15 @@ But what if you need to run your `react`, `vue` or any other javascript scripts.
 nikandlv@nikandlv.ir:~$ cd Postman
 nikandlv@nikandlv.ir:~/Postman$ git clone https://github.com/nikandlv/postman-patcher.git .
 nikandlv@nikandlv.ir:~/Postman$ python3 patch.py
+Patching RequesterModalContainer.js
+-- loading needle for RequesterModalContainer.js
+-- loading patch for RequesterModalContainer.js
+Backing up RequesterModalContainer.js -> RequesterModalContainer.js.back
+Patching vendor-shared.js
+-- loading needle for vendor-shared.js
+-- loading patch for vendor-shared.js
+-- loading needle 1 for vendor-shared.js
+-- loading patch 1 for vendor-shared.js
+Backing up vendor-shared.js -> vendor-shared.js.back
+nikandlv@nikandlv.ir:~/Postman$
 ```
--- patch.py
+++ patch.py
@@ -2,19 +2,59 @@ import os.path
 from shutil import copyfile
 import fileinput
 
+# Patch RequesterModalContainer
 RequesterModalContainer = './app/resources/app/js/RequesterModalContainer.js'
 RequesterModalContainerBackup = RequesterModalContainer + '.back'
 print('Patching RequesterModalContainer.js')
 if os.path.isfile(RequesterModalContainer):
+
   print('-- loading needle for RequesterModalContainer.js')
   with open('./patch/app/resources/app/js/RequesterModalContainer.js.needle', 'r') as file:
       requester_modal_container_needle = file.read()
+
   print('-- loading patch for RequesterModalContainer.js')
   with open('./patch/app/resources/app/js/RequesterModalContainer.js.patch', 'r') as file:
       requester_modal_container_patch = file.read()
+
   print('Backing up RequesterModalContainer.js -> RequesterModalContainer.js.back')
   copyfile(RequesterModalContainer, RequesterModalContainerBackup)
+
   for line in fileinput.input([RequesterModalContainer], inplace=True):
     print(line.replace(requester_modal_container_needle, requester_modal_container_patch), end='')
+
 else:
   print('RequesterModalContainer.js Not found!')
+
+# Patch 
+VendorShared = './app/resources/app/js/vendor-shared.js'
+VendorSharedBackup = VendorShared + '.back'
+print('Patching vendor-shared.js')
+if os.path.isfile(VendorShared):
+
+  print('-- loading needle for vendor-shared.js')
+  with open('./patch/app/resources/app/js/vendor-shared.js.needle', 'r') as file:
+      vendor_shared_needle = file.read()
+
+  print('-- loading patch for vendor-shared.js')
+  with open('./patch/app/resources/app/js/vendor-shared.js.patch', 'r') as file:
+      vendor_shared_patch = file.read()
+  
+  print('-- loading needle 1 for vendor-shared.js')
+  with open('./patch/app/resources/app/js/vendor-shared.js.1.needle', 'r') as file:
+      vendor_shared_needle_1 = file.read()
+
+  print('-- loading patch 1 for vendor-shared.js')
+  with open('./patch/app/resources/app/js/vendor-shared.js.1.patch', 'r') as file:
+      vendor_shared_patch_1 = file.read()  
+
+  print('Backing up vendor-shared.js -> vendor-shared.js.back')
+  copyfile(VendorShared, VendorSharedBackup)
+
+  for line in fileinput.input([VendorShared], inplace=True):
+    print(line.replace(vendor_shared_needle, vendor_shared_patch), end='')
+
+  for line in fileinput.input([VendorShared], inplace=True):
+    print(line.replace(vendor_shared_needle_1, vendor_shared_patch_1), end='')
+
+else:
+  print('vendor-shared.js Not found!')
